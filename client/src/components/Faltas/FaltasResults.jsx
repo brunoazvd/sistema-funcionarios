0078aebe0512896f6ed3756c30fd243b182da0a2
Bruno Azevedo <onurbazv@gmail.com>
2025-06-27 21:10:15 -0300

    fixes

--- client/src/components/Faltas/FaltasResults.jsx
+++ client/src/components/Faltas/FaltasResults.jsx
@@ -4,90 +4,80 @@ import { AlertDialog } from "@base-ui-components/react/alert-dialog";
 import { useState } from "react";
 
 const FaltasResults = ({ results, deleteAction, setModalContent }) => {
-	const [deleteId, setDeleteId] = useState(null);
+  const [deleteId, setDeleteId] = useState(null);
 
-	return (
-		results.length > 0 && (
-			<>
-				<AlertDialog.Root>
-					<table className="text-black table-auto text-left">
-						<thead className="bg-indigo-500 text-indigo-50">
-							<tr>
-								<th className="p-2 w-4/12">Funcionário</th>
-								<th className="p-2 w-1/12">Data</th>
-								<th className="p-2 w-6/12">Observação</th>
-								<th className="p-2 w-1/12">Ações</th>
-							</tr>
-						</thead>
-						<tbody>
-							{results.map((falta, index) => {
-								return (
-									<tr key={index} className="bg-indigo-50">
-										<td className="px-2 py-1">
-											{falta.funcionario.nome}
-										</td>
-										<td className="px-2 py-1">
-											{new Date(
-												falta.data
-											).toLocaleDateString("pt-BR", {
-												timeZone: "UTC",
-											})}
-										</td>
-										<td className="px-2 py-1">
-											{falta.observacao
-												? falta.observacao
-												: ""}
-										</td>
-										<td className="px-2 py-1">
-											<button
-												className="mr-4 cursor-pointer hover:text-amber-600"
-												type="button"
-												onClick={() =>
-													setModalContent(falta)
-												}
-											>
-												<EditIcon />
-											</button>
-											<AlertDialog.Trigger
-												className="mr-4 cursor-pointer hover:text-red-600"
-												onClick={() =>
-													setDeleteId(falta.id)
-												}
-											>
-												<DeleteIcon />
-											</AlertDialog.Trigger>
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
-					<AlertDialog.Portal>
-						<AlertDialog.Backdrop className="fixed inset-0 bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-50" />
-						<AlertDialog.Popup className="fixed top-1/2 left-1/2 -mt-8 min-w-sm max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-indigo-100 p-6 text-black outline outline-indigo-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0">
-							<AlertDialog.Title className="-mt-1.5 mb-1 text-lg font-medium">
-								Excluir Falta?
-							</AlertDialog.Title>
-							<AlertDialog.Description className="mb-6 text-base text-gray-600">
-								Você não pode desfazer esta ação!
-							</AlertDialog.Description>
-							<div className="flex gap-4">
-								<AlertDialog.Close className="flex h-8 items-center justify-center bg-gray-50 px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-200 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100">
-									Cancelar
-								</AlertDialog.Close>
-								<AlertDialog.Close
-									onClick={() => deleteAction(deleteId)}
-									className="flex h-8 items-center justify-center text-red-800 bg-gray-50 px-3.5 text-base font-medium hover:text-gray-900 hover:bg-red-400 focus-visible:-outline-offset-1 focus-visible:outline-red-800 active:bg-gray-100"
-								>
-									Excluir
-								</AlertDialog.Close>
-							</div>
-						</AlertDialog.Popup>
-					</AlertDialog.Portal>
-				</AlertDialog.Root>
-			</>
-		)
-	);
+  return (
+    results.length > 0 && (
+      <>
+        <AlertDialog.Root>
+          <table className="text-black table-auto text-left">
+            <thead className="bg-indigo-500 text-indigo-50">
+              <tr>
+                <th className="p-2 w-4/12">Funcionário</th>
+                <th className="p-2 w-1/12">Data</th>
+                <th className="p-2 w-6/12">Observação</th>
+                <th className="p-2 w-1/12">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {results.map((falta, index) => {
+                return (
+                  <tr key={index} className="bg-indigo-50">
+                    <td className="px-2 py-1">{falta.funcionario.nome}</td>
+                    <td className="px-2 py-1">
+                      {new Date(falta.data).toLocaleDateString("pt-BR", {
+                        timeZone: "UTC",
+                      })}
+                    </td>
+                    <td className="px-2 py-1">
+                      {falta.observacao ? falta.observacao : ""}
+                    </td>
+                    <td className="px-2 py-1">
+                      <button
+                        className="mr-4 cursor-pointer hover:text-green-600"
+                        type="button"
+                        onClick={() => setModalContent(falta)}
+                      >
+                        <EditIcon />
+                      </button>
+                      <AlertDialog.Trigger
+                        className="mr-4 cursor-pointer hover:text-red-600"
+                        onClick={() => setDeleteId(falta.id)}
+                      >
+                        <DeleteIcon />
+                      </AlertDialog.Trigger>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <AlertDialog.Portal>
+            <AlertDialog.Backdrop className="fixed inset-0 bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-50" />
+            <AlertDialog.Popup className="fixed top-1/2 left-1/2 -mt-8 min-w-sm max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-indigo-100 p-6 text-black outline outline-indigo-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0">
+              <AlertDialog.Title className="-mt-1.5 mb-1 text-lg font-medium">
+                Excluir Falta?
+              </AlertDialog.Title>
+              <AlertDialog.Description className="mb-6 text-base text-gray-600">
+                Você não pode desfazer esta ação!
+              </AlertDialog.Description>
+              <div className="flex gap-4">
+                <AlertDialog.Close className="flex h-8 items-center justify-center bg-gray-50 px-3.5 text-base font-medium text-gray-900 select-none hover:bg-gray-200 focus-visible:-outline-offset-1 focus-visible:outline-blue-800 active:bg-gray-100">
+                  Cancelar
+                </AlertDialog.Close>
+                <AlertDialog.Close
+                  onClick={() => deleteAction(deleteId)}
+                  className="flex h-8 items-center justify-center text-red-800 bg-gray-50 px-3.5 text-base font-medium hover:text-gray-900 hover:bg-red-400 focus-visible:-outline-offset-1 focus-visible:outline-red-800 active:bg-gray-100"
+                >
+                  Excluir
+                </AlertDialog.Close>
+              </div>
+            </AlertDialog.Popup>
+          </AlertDialog.Portal>
+        </AlertDialog.Root>
+      </>
+    )
+  );
 };
 
 export default FaltasResults;
--- server/src/api/services/AtestadoService.js
+++ server/src/api/services/AtestadoService.js
@@ -1,123 +1,126 @@
 import prisma from "../../database/index.js";
 
 class AtestadoService {
-    constructor() {
-        this.prisma = prisma;
+  constructor() {
+    this.prisma = prisma;
+  }
+
+  async create(data) {
+    try {
+      const newAtestado = await this.prisma.atestado.create({
+        data,
+      });
+      return newAtestado;
+    } catch (error) {
+      throw new Error(`Erro ao criar atestado: ${error.message}`);
     }
+  }
 
-    async create(data) {
-        try {
-            const newAtestado = await this.prisma.atestado.create({
-                data
-            });
-            return newAtestado;
-        } catch (error) {
-            throw new Error(`Erro ao criar atestado: ${error.message}`);
-        }
+  async delete(id) {
+    try {
+      const deletedAtestado = await this.prisma.atestado.delete({
+        where: { id },
+      });
+
+      return deletedAtestado;
+    } catch (error) {
+      throw new Error(`Erro ao deletar atestado: ${error.message}`);
     }
+  }
 
-    async delete(id) {
-        try {
-            const deletedAtestado = await this.prisma.atestado.delete({
-                where: { id }
-            });
+  async update(id, updates) {
+    try {
+      const updatedAtestado = await this.prisma.atestado.update({
+        where: { id },
+        data: updates,
+        include: {
+          funcionario: {
+            select: {
+              nome: true,
+            },
+          },
+        },
+      });
 
-            return deletedAtestado;
-        } catch (error) {
-            throw new Error(`Erro ao deletar atestado: ${error.message}`);
-        }
+      return updatedAtestado;
+    } catch (error) {
+      throw new Error(`Erro ao atualizar atestado: ${error.message}`);
     }
+  }
 
-    async update(id, updates) {
-        try {
-            const updatedAtestado = await this.prisma.atestado.update({
-                where: { id },
-                data: updates,
-                include: {
-                    funcionario: {
-                        select: {
-                            nome: true,
-                        },
-                    }
-                }
-            });
-
-            return updatedAtestado;
-        } catch (error) {
-            throw new Error(`Erro ao atualizar atestado: ${error.message}`);
-        }
+  async getAll() {
+    try {
+      const atestados = await this.prisma.atestado.findMany({});
+      return atestados;
+    } catch (error) {
+      throw new Error(`Erro ao buscar atestados: ${error.message}`);
     }
+  }
 
-    async getAll() {
-        try {
-            const atestados = await this.prisma.atestado.findMany({});
-            return atestados;
-        } catch (error) {
-            throw new Error(`Erro ao buscar atestados: ${error.message}`);
-        }
+  async getById(id) {
+    try {
+      const atestado = await this.prisma.atestado.findUnique({
+        where: { id },
+      });
+
+      return atestado;
+    } catch (error) {
+      throw new Error(`Erro ao buscar atestado: ${error.message}`);
     }
+  }
 
-    async getById(id) {
-        try {
-            const atestado = await this.prisma.atestado.findUnique({
-                where: { id }
-            });
+  async getByFuncionario(id) {
+    try {
+      const atestados = await this.prisma.atestado.findMany({
+        where: { funcionarioId: id },
+      });
 
-            return atestado;
-        } catch (error) {
-            throw new Error(`Erro ao buscar atestado: ${error.message}`);
-        }
+      return atestados;
+    } catch (error) {
+      throw new Error(`Erro ao buscar atestados: ${error.message}`);
     }
+  }
 
-    async getByFuncionario(id) {
-        try {
-            const atestados = await this.prisma.atestado.findMany({
-                where: { funcionarioId: id }
-            });
+  async search(initialDate, finalDate, funcionario) {
+    try {
+      const where = {};
+      if (initialDate || finalDate) {
+        where.data = {};
+        initialDate && (where.data.gte = new Date(initialDate));
+        finalDate && (where.data.lte = new Date(finalDate));
+      }
 
-            return atestados;
-        } catch (error) {
-            throw new Error(`Erro ao buscar atestados: ${error.message}`);
-        }
+      if (funcionario) {
+        where.funcionario = {
+          nome: {
+            contains: funcionario,
+          },
+        };
+      }
+
+      if (Object.keys(where).length === 0) {
+        return [];
+      }
+
+      const atestados = await this.prisma.atestado.findMany({
+        where,
+        include: {
+          funcionario: {
+            select: {
+              nome: true,
+            },
+          },
+        },
+        orderBy: {
+          data: "desc",
+        },
+      });
+
+      return atestados;
+    } catch (error) {
+      throw new Error(`Erro ao buscar atestados: ${error.message}`);
     }
-
-    async search(initialDate, finalDate, funcionario) {
-        try {
-            const where = {}
-            if (initialDate || finalDate) {
-                where.data = {}
-                initialDate && (where.data.gte = new Date(initialDate))
-                finalDate && (where.data.lte = new Date(finalDate))
-            }
-
-            if (funcionario) {
-                where.funcionario = {
-                    nome: {
-                        contains: funcionario,
-                    },
-                }
-            }
-
-            if (Object.keys(where).length === 0) {
-                return [];
-            }
-
-            const atestados = await this.prisma.atestado.findMany({
-                where,
-                include: {
-                    funcionario: {
-                        select: {
-                            nome: true,
-                        },
-                    }
-                }
-            });
-
-            return atestados;
-        } catch (error) {
-            throw new Error(`Erro ao buscar atestados: ${error.message}`);
-        }
-    }
+  }
 }
 
 const service = new AtestadoService();
