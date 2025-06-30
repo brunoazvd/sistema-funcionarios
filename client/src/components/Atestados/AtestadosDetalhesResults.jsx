f5a19e3dc26c775a359bc71338eb9e7dfcad29f5
Bruno Azevedo <onurbazv@gmail.com>
2025-06-30 17:45:12 -0300

    fixed Dialog.Root nesting

--- client/src/components/Atestados/AtestadosDetalhesResults.jsx
+++ client/src/components/Atestados/AtestadosDetalhesResults.jsx
@@ -5,108 +5,80 @@ import DeleteIcon from "../icons/DeleteIcon.jsx";
 
 const AtestadosDetalhesResults = ({
 	results,
-	deleteAction,
+	handleOpenAlert,
+	alertType,
 	setModalContent = () => null,
 }) => {
-	const [deleteId, setDeleteId] = useState(null);
-
 	return results.length > 0 ? (
 		<>
-			<AlertDialog.Root>
-				<table className="text-black table-auto text-left w-full">
-					<thead className="bg-indigo-500 text-indigo-50">
-						<tr>
-							<th className="p-2 w-1/10">Data</th>
-							<th className="p-2 w-2/10">Tipo</th>
-							<th className="p-2 w-1/10">Dias</th>
-							<th className="p-2 w-3/10">Observação</th>
-							<th className="p-2 w-2/10">Ações</th>
-						</tr>
-					</thead>
-					<tbody>
-						{results.map((atestado, index) => (
-							<tr key={index} className="bg-indigo-50">
-								<td className="px-2 py-1">
-									{new Date(atestado.data).toLocaleDateString(
-										"pt-BR",
-										{
-											timeZone: "UTC",
+			<table className="text-black table-auto text-left w-full">
+				<thead className="bg-indigo-500 text-indigo-50">
+					<tr>
+						<th className="p-2 w-1/10">Data</th>
+						<th className="p-2 w-2/10">Tipo</th>
+						<th className="p-2 w-1/10">Dias</th>
+						<th className="p-2 w-3/10">Observação</th>
+						<th className="p-2 w-2/10">Ações</th>
+					</tr>
+				</thead>
+				<tbody>
+					{results.map((atestado, index) => (
+						<tr key={index} className="bg-indigo-50">
+							<td className="px-2 py-1">
+								{new Date(atestado.data).toLocaleDateString(
+									"pt-BR",
+									{
+										timeZone: "UTC",
+									}
+								)}
+							</td>
+							<td className="px-2 py-1">
+								{atestado.tipo === "COMPARECIMENTO"
+									? "Comparecimento"
+									: "Atestado Médico"}
+							</td>
+							<td className="px-2 py-1">
+								{atestado.dias === 0 ? "" : atestado.dias}
+							</td>
+							<td className="px-2 py-1">
+								{atestado.observacao ? atestado.observacao : ""}
+							</td>
+							<td className="px-2 py-1">
+								{setModalContent && (
+									<button
+										className="mr-4 cursor-pointer hover:text-green-600"
+										type="button"
+										onClick={() =>
+											setModalContent(atestado)
 										}
-									)}
-								</td>
-								<td className="px-2 py-1">
-									{atestado.tipo === "COMPARECIMENTO"
-										? "Comparecimento"
-										: "Atestado Médico"}
-								</td>
-								<td className="px-2 py-1">
-									{atestado.dias === 0 ? "" : atestado.dias}
-								</td>
-								<td className="px-2 py-1">
-									{atestado.observacao
-										? atestado.observacao
-										: ""}
-								</td>
-								<td className="px-2 py-1">
-									{setModalContent && (
-										<button
-											className="mr-4 cursor-pointer hover:text-green-600"
-											type="button"
-											onClick={() =>
-												setModalContent(atestado)
-											}
-										>
-											<div className="flex">
-												<EditIcon />
-												<span className="text-xs">
-													Editar
-												</span>
-											</div>
-										</button>
-									)}
-									{deleteAction && (
-										<AlertDialog.Trigger
-											className="cursor-pointer hover:text-red-600"
-											onClick={() =>
-												setDeleteId(atestado.id)
-											}
-										>
-											<div className="flex">
-												<DeleteIcon />
-												<span className="text-xs">
-													Excluir
-												</span>
-											</div>
-										</AlertDialog.Trigger>
-									)}
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
-				<AlertDialog.Portal>
-					<AlertDialog.Backdrop className="fixed inset-0 bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-50 z-80" />
-					<AlertDialog.Popup className="fixed top-1/2 left-1/2 -mt-8 min-w-sm max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-indigo-100 p-6 text-black outline outline-indigo-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0">
-						<AlertDialog.Title className="-mt-1.5 mb-1 text-lg font-medium z-90">
-							Excluir Atestado?
-						</AlertDialog.Title>
-						<AlertDialog.Description className="mb-6 text-base text-gray-600">
-							Você não pode desfazer esta ação!
-						</AlertDialog.Description>
-						<div className="flex justify-end gap-3">
-							<AlertDialog.Close className="rounded-md bg-indigo-50 px-4 py-2 font-medium text-indigo-900 hover:bg-indigo-100">
-								Cancelar
-							</AlertDialog.Close>
-							<AlertDialog.Close
-								className="rounded-md bg-red-500 px-4 py-2 font-medium text-white hover:bg-red-600"
-								onClick={() => deleteAction(deleteId)}
-							>
-								Excluir
-							</AlertDialog.Close>
-						</div>
-					</AlertDialog.Popup>
-				</AlertDialog.Portal>
-			</AlertDialog.Root>
+									>
+										<div className="flex">
+											<EditIcon />
+											<span className="text-xs">
+												Editar
+											</span>
+										</div>
+									</button>
+								)}
+								<button
+									className="cursor-pointer hover:text-red-600"
+									onClick={() =>
+										handleOpenAlert(
+											Number(atestado.id),
+											alertType
+										)
+									}
+								>
+									<div className="flex">
+										<DeleteIcon />
+										<span className="text-xs">Excluir</span>
+									</div>
+								</button>
+							</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</>
 	) : (
 		<p className="text-center py-4 text-gray-600">
--- client/src/components/Funcionarios/FuncionarioDetalhes.jsx
+++ client/src/components/Funcionarios/FuncionarioDetalhes.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 import { Tabs } from "@base-ui-components/react/tabs";
 import { Dialog } from "@base-ui-components/react/dialog";
+import { AlertDialog } from "@base-ui-components/react/alert-dialog";
 
 import { buscarFaltasPorFuncionarioId } from "../../services/api/faltas";
 import { buscarAtestadosPorFuncionarioId } from "../../services/api/atestados";
@@ -24,8 +25,11 @@ const FuncionarioDetalhes = ({
 	const [atestados, setAtestados] = useState([]);
 	const [faltaModalOpen, setFaltaModalOpen] = useState(false);
 	const [atestadoModalOpen, setAtestadoModalOpen] = useState(false);
+	const [alertOpen, setAlertOpen] = useState(false);
 	const [currentFalta, setCurrentFalta] = useState(null);
 	const [currentAtestado, setCurrentAtestado] = useState(null);
+	const [deleteId, setDeleteId] = useState(0);
+	const [alertType, setAlertType] = useState("falta");
 
 	useEffect(() => {
 		if (funcionarioId && isOpen) {
@@ -70,6 +74,12 @@ const FuncionarioDetalhes = ({
 		);
 	};
 
+	const handleOpenAlert = (id, type) => {
+		setDeleteId(id);
+		setAlertType(type);
+		setAlertOpen(true);
+	};
+
 	return (
 		<>
 			<Dialog.Root open={isOpen} onOpenChange={onClose}>
@@ -98,7 +108,8 @@ const FuncionarioDetalhes = ({
 							<Tabs.Panel value="faltas">
 								<FaltasDetalhesResults
 									results={faltas}
-									deleteAction={handleDeleteFalta}
+									handleOpenAlert={handleOpenAlert}
+									alertType="falta"
 									setModalContent={(falta) => {
 										setCurrentFalta(falta);
 										setFaltaModalOpen(true);
@@ -108,7 +119,8 @@ const FuncionarioDetalhes = ({
 							<Tabs.Panel value="atestados">
 								<AtestadosDetalhesResults
 									results={atestados}
-									deleteAction={handleDeleteAtestado}
+									handleOpenAlert={handleOpenAlert}
+									alertType="atestado"
 									setModalContent={(atestado) => {
 										setCurrentAtestado(atestado);
 										setAtestadoModalOpen(true);
@@ -161,6 +173,39 @@ const FuncionarioDetalhes = ({
 					</Dialog.Popup>
 				</Dialog.Portal>
 			</Dialog.Root>
+
+			{/* Modal de confirmação de exclusão de Faltas e Atestados */}
+			<AlertDialog.Root open={alertOpen} onOpenChange={setAlertOpen}>
+				<AlertDialog.Portal>
+					<AlertDialog.Backdrop className="fixed inset-0 bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-50" />
+					<AlertDialog.Popup className="fixed top-1/2 left-1/2 -mt-8 min-w-sm max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 rounded-lg bg-indigo-100 p-6 text-black outline outline-indigo-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0">
+						<AlertDialog.Title className="-mt-1.5 mb-1 text-lg font-medium">
+							Excluir{" "}
+							{alertType === "falta" ? "Falta" : "Atestado"}?
+						</AlertDialog.Title>
+						<AlertDialog.Description className="mb-6 text-base text-gray-600">
+							Você não pode desfazer esta ação!
+						</AlertDialog.Description>
+						<div className="flex justify-end gap-3">
+							<AlertDialog.Close className="rounded-md bg-indigo-50 px-4 py-2 font-medium text-indigo-900 hover:bg-indigo-100">
+								Cancelar
+							</AlertDialog.Close>
+							<AlertDialog.Close
+								className="rounded-md bg-red-500 px-4 py-2 font-medium text-white hover:bg-red-600"
+								onClick={() => {
+									if (alertType === "falta") {
+										handleDeleteFalta(deleteId);
+									} else {
+										handleDeleteAtestado(deleteId);
+									}
+								}}
+							>
+								Excluir
+							</AlertDialog.Close>
+						</div>
+					</AlertDialog.Popup>
+				</AlertDialog.Portal>
+			</AlertDialog.Root>
 		</>
 	);
 };
