ff3db8f3eaca9440bc1e0cbc799e46b47770db79
Bruno Azevedo <onurbazv@gmail.com>
2025-07-02 16:53:22 -0300

    sticky tables

--- client/src/components/Faltas/FaltasResults.jsx
+++ client/src/components/Faltas/FaltasResults.jsx
@@ -10,68 +10,73 @@ const FaltasResults = ({ results, deleteAction, setModalContent }) => {
 		results.length > 0 && (
 			<>
 				<AlertDialog.Root>
-					<table className="text-black table-auto text-left">
-						<thead className="bg-indigo-500 text-indigo-50">
-							<tr>
-								<th className="p-2 w-4/12">Funcionário</th>
-								<th className="p-2 w-1/12">Data</th>
-								<th className="p-2 w-5/12">Observação</th>
-								<th className="p-2 w-2/12">Ações</th>
-							</tr>
-						</thead>
-						<tbody>
-							{results.map((falta, index) => {
-								return (
-									<tr key={index} className="bg-indigo-50">
-										<td className="px-2 py-1">
-											{falta.funcionario.nome}
-										</td>
-										<td className="px-2 py-1">
-											{new Date(
-												falta.data
-											).toLocaleDateString("pt-BR", {
-												timeZone: "UTC",
-											})}
-										</td>
-										<td className="px-2 py-1">
-											{falta.observacao
-												? falta.observacao
-												: ""}
-										</td>
-										<td className="px-2 py-1">
-											<button
-												className="mr-4 cursor-pointer hover:text-green-600"
-												type="button"
-												onClick={() =>
-													setModalContent(falta)
-												}
-											>
-												<div className="flex">
-													<EditIcon />
-													<span className="text-xs">
-														Editar
-													</span>
-												</div>
-											</button>
-											<AlertDialog.Trigger
-												className="mr-4 cursor-pointer hover:text-red-600"
-												onClick={() =>
-													setDeleteId(falta.id)
-												}
-											>
-												<div className="flex">
-													<DeleteIcon />
-													<span className="text-xs">
-														Excluir
-													</span>
-												</div>
-											</AlertDialog.Trigger>
-										</td>
-									</tr>
-								);
-							})}
-						</tbody>
-					</table>
+					<div className="max-h-160 overflow-y-auto">
+						<table className="text-black table-auto text-left">
+							<thead className="bg-indigo-500 text-indigo-50 sticky top-0">
+								<tr>
+									<th className="p-2 w-4/12">Funcionário</th>
+									<th className="p-2 w-1/12">Data</th>
+									<th className="p-2 w-5/12">Observação</th>
+									<th className="p-2 w-2/12">Ações</th>
+								</tr>
+							</thead>
+							<tbody>
+								{results.map((falta, index) => {
+									return (
+										<tr
+											key={index}
+											className="bg-indigo-50"
+										>
+											<td className="px-2 py-1">
+												{falta.funcionario.nome}
+											</td>
+											<td className="px-2 py-1">
+												{new Date(
+													falta.data
+												).toLocaleDateString("pt-BR", {
+													timeZone: "UTC",
+												})}
+											</td>
+											<td className="px-2 py-1">
+												{falta.observacao
+													? falta.observacao
+													: ""}
+											</td>
+											<td className="px-2 py-1">
+												<button
+													className="mr-4 cursor-pointer hover:text-green-600"
+													type="button"
+													onClick={() =>
+														setModalContent(falta)
+													}
+												>
+													<div className="flex">
+														<EditIcon />
+														<span className="text-xs">
+															Editar
+														</span>
+													</div>
+												</button>
+												<AlertDialog.Trigger
+													className="mr-4 cursor-pointer hover:text-red-600"
+													onClick={() =>
+														setDeleteId(falta.id)
+													}
+												>
+													<div className="flex">
+														<DeleteIcon />
+														<span className="text-xs">
+															Excluir
+														</span>
+													</div>
+												</AlertDialog.Trigger>
+											</td>
+										</tr>
+									);
+								})}
+							</tbody>
+						</table>
+					</div>
 					<AlertDialog.Portal>
 						<AlertDialog.Backdrop className="fixed inset-0 bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-50" />
 						<AlertDialog.Popup className="fixed top-1/2 left-1/2 -mt-8 min-w-sm max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 bg-indigo-100 p-6 text-black outline outline-indigo-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0">
--- client/src/components/Funcionarios/FuncionariosResults.jsx
+++ client/src/components/Funcionarios/FuncionariosResults.jsx
@@ -21,74 +21,80 @@ const FuncionariosResults = ({
 		results.length > 0 && (
 			<>
 				<AlertDialog.Root>
-					<table className="text-black table-auto text-left w-full">
-						<thead className="bg-indigo-500 text-indigo-50">
-							<tr>
-								<th className="p-2 w-4/10">Nome</th>
-								<th className="p-2 w-2/10">Cargo</th>
-								<th className="p-2 w-2/10">Telefone</th>
-								<th className="p-2 w-2/10">Ações</th>
-							</tr>
-						</thead>
-						<tbody>
-							{results.map((funcionario, index) => (
-								<tr key={index} className="bg-indigo-50">
-									<td className="px-2 py-1">
-										{funcionario.nome}
-									</td>
-									<td className="px-2 py-1">
-										{cargosMap.get(funcionario.cargo)}
-									</td>
-									<td className="px-2 py-1">
-										{formatTelefone(funcionario.telefone)}
-									</td>
-									<td className="px-2 py-1">
-										<button
-											className="mr-4 cursor-pointer hover:text-green-600"
-											type="button"
-											onClick={() =>
-												setModalContent(funcionario)
-											}
-										>
-											<div className="flex gap-0.5">
-												<EditIcon />
-												<span className="text-xs">
-													Editar
-												</span>
-											</div>
-										</button>
-										<AlertDialog.Trigger
-											className="mr-4 cursor-pointer hover:text-red-600"
-											onClick={() =>
-												setDeleteId(funcionario.id)
-											}
-										>
-											<div className="flex gap-0.5">
-												<DeleteIcon />
-												<span className="text-xs">
-													Excluir
-												</span>
-											</div>
-										</AlertDialog.Trigger>
-										<button
-											className="mr-4 cursor-pointer hover:text-indigo-600"
-											type="button"
-											onClick={() =>
-												handleViewDetails(funcionario)
-											}
-										>
-											<div className="flex gap-0.5">
-												<DetailsIcon />
-												<span className="text-xs">
-													Detalhes
-												</span>
-											</div>
-										</button>
-									</td>
+					<div className="max-h-160 overflow-y-auto">
+						<table className="text-black table-auto text-left w-full">
+							<thead className="bg-indigo-500 text-indigo-50 sticky top-0">
+								<tr>
+									<th className="p-2 w-4/10">Nome</th>
+									<th className="p-2 w-2/10">Cargo</th>
+									<th className="p-2 w-2/10">Telefone</th>
+									<th className="p-2 w-2/10">Ações</th>
 								</tr>
-							))}
-						</tbody>
-					</table>
+							</thead>
+							<tbody>
+								{results.map((funcionario, index) => (
+									<tr key={index} className="bg-indigo-50">
+										<td className="px-2 py-1">
+											{funcionario.nome}
+										</td>
+										<td className="px-2 py-1">
+											{cargosMap.get(funcionario.cargo)}
+										</td>
+										<td className="px-2 py-1">
+											{formatTelefone(
+												funcionario.telefone
+											)}
+										</td>
+										<td className="px-2 py-1">
+											<button
+												className="mr-4 cursor-pointer hover:text-green-600"
+												type="button"
+												onClick={() =>
+													setModalContent(funcionario)
+												}
+											>
+												<div className="flex gap-0.5">
+													<EditIcon />
+													<span className="text-xs">
+														Editar
+													</span>
+												</div>
+											</button>
+											<AlertDialog.Trigger
+												className="mr-4 cursor-pointer hover:text-red-600"
+												onClick={() =>
+													setDeleteId(funcionario.id)
+												}
+											>
+												<div className="flex gap-0.5">
+													<DeleteIcon />
+													<span className="text-xs">
+														Excluir
+													</span>
+												</div>
+											</AlertDialog.Trigger>
+											<button
+												className="mr-4 cursor-pointer hover:text-indigo-600"
+												type="button"
+												onClick={() =>
+													handleViewDetails(
+														funcionario
+													)
+												}
+											>
+												<div className="flex gap-0.5">
+													<DetailsIcon />
+													<span className="text-xs">
+														Detalhes
+													</span>
+												</div>
+											</button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
+					</div>
 					<AlertDialog.Portal>
 						<AlertDialog.Backdrop className="fixed inset-0 bg-black opacity-20 transition-all duration-150 data-[ending-style]:opacity-0 data-[starting-style]:opacity-0 dark:opacity-50 z-30" />
 						<AlertDialog.Popup className="fixed top-1/2 left-1/2 -mt-8 min-w-sm max-w-[calc(100vw-3rem)] -translate-x-1/2 -translate-y-1/2 bg-indigo-100 p-6 text-black outline outline-indigo-200 transition-all duration-150 data-[ending-style]:scale-90 data-[ending-style]:opacity-0 data-[starting-style]:scale-90 data-[starting-style]:opacity-0 z-40">
