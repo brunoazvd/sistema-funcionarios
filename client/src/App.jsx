fec6f83e88564b223bdbc9df9258ddf8ea78c263
Bruno Azevedo <onurbazv@gmail.com>
2025-07-02 17:02:02 -0300

    Removed Loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
-import { Suspense } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { Router } from "./general/Router.jsx";
-import { Loading } from "./general/Loading.jsx";
 import { FuncionariosProvider } from "./contexts/FuncionariosContext.jsx";
 import { Toast } from "@base-ui-components/react/toast";
 
@@ -9,11 +7,9 @@ export const App = () => (
 	<BrowserRouter>
 		<FuncionariosProvider>
 			<Toast.Provider>
-				<Suspense fallback={<Loading name="suspense" />}>
-					<div className="min-h-screen bg-indigo-950 text-indigo-50 pb-8">
-						<Router />
-					</div>
-				</Suspense>
+				<div className="min-h-screen bg-indigo-950 text-indigo-50 pb-8">
+					<Router />
+				</div>
 			</Toast.Provider>
 		</FuncionariosProvider>
 	</BrowserRouter>
