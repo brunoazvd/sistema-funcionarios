af8529c18c7995627afce926ddbe2be38fff41c5
Bruno Azevedo <onurbazv@gmail.com>
2025-08-11 12:09:19 -0300

    ready for prod

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import apiRoutes from "./src/api/routes.js";
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.use(apiRoutes);
 
-app.listen(3000, "0.0.0.0", () => {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(3005, "0.0.0.0", () => {
     console.log("Server is running");
 });
